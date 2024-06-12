2acddc5f315a4c55681e806908305b9b6ba2186e
Aleksandr <donato700@icloud.com>
2024-06-12 20:09:58 +0500

    Сделал через Gorilla Mux. Сделать мидлвары аутентификацию, счётчик обращение, логер

--- main.go
+++ main.go
@@ -4,13 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	//"github.com/gorilla/mux"
+
+	"github.com/gorilla/mux"
 )
 
 var stormaps = make(map[string]string, 10)
 
 func Record(date string, affairs string, stormaps map[string]string) {
 	stormaps[date] = affairs
+	fmt.Println("Новая запись в ежедневнике")
+}
+
+func PrintStoremaps(stormaps map[string]string) {
+	for key, value := range stormaps {
+		fmt.Println(key, " : ", value)
+	}
 }
 
 func diaryHandler(w http.ResponseWriter, r *http.Request) {
@@ -27,13 +35,19 @@ func diaryHandler(w http.ResponseWriter, r *http.Request) {
 
 	Record(date, affairs, stormaps)
 
-	fmt.Println(stormaps)
+	PrintStoremaps(stormaps)
+}
+
+func setHandlers(r *mux.Router) {
+	r.HandleFunc("/diary", diaryHandler)
 }
 
 func main() {
-	http.HandleFunc("/diary", diaryHandler)
+	r := mux.NewRouter()
+	setHandlers(r)
+
 	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatal(err)
 	}
 }
