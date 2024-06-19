e798c11e23c2e08df27e028105359a97668ee4b7
Aleksandr <donato700@icloud.com>
2024-06-19 23:01:48 +0500

    сделал мидлвару через Use (переписал под сигнатуру). Сделать логирование и раскидать по папкам

--- main.go
+++ main.go
@@ -15,41 +15,50 @@ var stormaps = make(map[string]string, 10) // Ежедневник
 // Запись в ежедневник
 func Record(date string, affairs string, stormaps map[string]string) {
 	stormaps[date] = affairs
-	fmt.Println(time.Now(), "Новая запись в ежедневнике =>", date, affairs)
+	//fmt.Println(time.Now(), "Новая запись в ежедневнике =>", date, affairs)
 }
 
 // Печать ежедневника
 func PrintStoremaps(w http.ResponseWriter, stormaps map[string]string) {
 	for key, value := range stormaps {
 		fmt.Fprintf(w, "%s : %v\n", key, value)
-		//fmt.Printf("%s : %v\n", key, value)
 	}
 }
 
 // Обработка на запись в ежедневник
 func diaryHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/diary" {
-		http.Error(w, "404 not found", http.StatusNotFound)
-		return
-	}
 	if r.Method != "POST" {
 		http.Error(w, "Method is not supported.", http.StatusNotFound)
 		return
 	}
 
-	affairs := r.FormValue("affairs")
-	date := r.FormValue("date")
+	affairs := r.URL.Query().Get("affairs")
+	date := r.URL.Query().Get("date")
 
 	Record(date, affairs, stormaps)
 }
 
 // Счётчик запросов
-func CounterMiddleware(callBack func(w http.ResponseWriter, r *http.Request), Counter map[string]int) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
+func CounterMiddleware(n http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		Counter[r.URL.String()]++
-		fmt.Println(Counter)
-		callBack(w, r)
+		n.ServeHTTP(w, r)
+	})
+}
+
+func CounterHandler(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintln(w, Counter)
+	for key, value := range Counter {
+		fmt.Fprintf(w, "%s : %d\n", key, value)
 	}
+}
+
+func LogMiddlleware(n http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(time.Now())
+		n.ServeHTTP(w, r)
+	})
+
 }
 
 // Аутентификация
@@ -80,22 +89,18 @@ func HelloAdminFunc(w http.ResponseWriter, r *http.Request) {
 
 }
 
-// Печать счётчика обращений
-func CounterHandler(w http.ResponseWriter, Counter map[string]int) {
-	fmt.Fprintln(w, Counter)
-}
-
+// Навешать мидлвару через Use - сделал CounterMiddleware/ Переписать мидлвару под сигнатуру  (дз) - сделал CounterMiddleware!!! //  логирование сделать как мидлвару!!!!
 func setHandlers(r *mux.Router) {
-
-	r.HandleFunc("/diary", CounterMiddleware(diaryHandler, Counter))
+	r.Use(CounterMiddleware)
+	r.Use(LogMiddlleware)
+	r.HandleFunc("/diary", diaryHandler)
 
 	admin := r.PathPrefix("/admin").Subrouter()
 	admin.Use(AuthCheckMiddleware)
 
-	admin.HandleFunc("/allaffairs", CounterMiddleware(HelloAdminFunc, Counter))
-	admin.HandleFunc("/counter", func(w http.ResponseWriter, r *http.Request) {
-		CounterHandler(w, Counter)
-	})
+	admin.HandleFunc("/allaffairs", HelloAdminFunc)
+	admin.HandleFunc("/counter", CounterHandler)
+
 }
 
 func main() {
